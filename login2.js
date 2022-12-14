f1d20871691ab2dae38a3a5240acf3f38e50776a
nethanmcqinn <119746112+nethanmcqinn@users.noreply.github.com>
2022-12-13 21:46:26 -1100

    Add files via upload

---
+++ login2.js
@@ -0,0 +1,22 @@
+function validate()
+{
+var username=document.getElementById("username").value;
+var password=document.getElementById("password").value;
+if(username=="admin"&& password=="user")
+
+
+
+{
+    alert("login succesfully");
+    return false;
+
+}
+
+else 
+
+{   alert("log in failed") 
+   return false; 
+
+}
+
+}
